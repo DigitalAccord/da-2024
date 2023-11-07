2f33ae1edfdde60e496e133654d49ac3432bf56b
rahul <raahul.decrypt@gmail.com>
2023-11-07 18:06:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+   <>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './footer.scss'
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+        <div className='container'>
+           <div className='row'>
+           <div className='col-md-4'>
+            <div className='talk-wrapper'>
+                <div className='talk-content'>
+               <h1>Let’s Talk.</h1>
+               <p>(07)355400017</p>
+               <p className='small-text'>Suite 30, Lvl 7 445 Upper Edward St Spring Hill, BRISBANE CITY 4000</p>
+
+                </div>
+            </div>
+            </div>
+            <div className='col-md-8'>
+               <div className='left-section-wraper'>
+                <div className='map-wraper'></div>
+                <div className='form-wrapper'>
+                   
+                </div>
+
+               </div>
+
+                </div>
+           </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Footer
