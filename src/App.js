d728116901b76b46120299ac2472a4684a0ed9a1
rahul <raahul.decrypt@gmail.com>
2023-11-08 18:18:12 +0530

    category page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Footer from './Footer/Footer';
+import Category from './component/category/Category';
 
 function App() {
   return (
    <>
+   <Category/>
    <Footer/>
    </>
   );
