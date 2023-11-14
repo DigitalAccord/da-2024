6cc10c6fa01dcf667446639f3ee13de17b03f3f0
Rohitdecrypt <rohit.decrypt@gmail.com>
2023-11-14 16:39:34 +0530

    my swipper

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
+
 import './App.css';
 import Footer from './Footer/Footer';
 import Craousel from './component/Craousel/Craousel';
+import CenterMode from './component/MySwiper/Swiper';
 import Category from './component/category/Category';
 import Solutions from './component/solution/Solutions';
 
 function App() {
   return (
    <>
-   <Solutions/>
-   <Craousel/>
-   <Category/>
-   <Footer/>
+   {/* <Solutions/> */}
+   <CenterMode/>
+   {/* <Craousel/> */}
+   {/* <Category/>
+   <Footer/> */}
    </>
   );
 }
