4258e9f71e6d4d4b4ed77918d3cd3d3c1e64f6fc
rahul <raahul.decrypt@gmail.com>
2023-11-21 18:26:44 +0530

    last work of tuesday

--- src/component/solution/Solutions.jsx
+++ src/component/solution/Solutions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React,{ Component } from 'react'
 import './solutions.scss'
 import arrow from "../../assets/arrow.png"
 import thumb from "../../assets/thumb.png"
@@ -7,88 +7,13 @@ import overflow from '../../assets/overflow.png'
 import round from '../../assets/round.png'
 import hover from '../../assets/hoverImg.png'
 import { FiArrowUpRight } from 'react-icons/fi';
-
+import HorizontalScroll from 'react-scroll-horizontal'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const Solutions = () => {
-  const [cardsData, setCardsData] = useState([
-    { id: 1, title: 'McCormacks', content: "DESIGN" },
-    { id: 2, title: 'GW', content: "SEO | DESIGN" },
-    
-  ]);
-  const [activeCardId, setActiveCardId] = useState(1);
-  const [cardId, setCardId] = useState(-1);
-  const [maxCard, setMaxCard] = useState(0);
-
-  const handleCardClick = (id) => {
-    setActiveCardId(id);
-  };
-
-  useEffect(() => {
-    setMaxCard(cardsData.length);
-  }, []);
-
-  const handleCardChange = (index) => {
-    setActiveCardId(index)
-  };
-
-  const settings = {
-
-    dots: false,
-    afterChange: handleCardChange,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    CenterMode: true,
-    slidesToScroll: 1,
-    initialSlide: 1,
-    // autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+  const child   = { width: `300em`, height: `100%`}
+  // const parent  = { width: `100%`, height: `100%`}
   return (
     <>
       <div className='solutions-wrapper'>
@@ -117,7 +42,7 @@ const Solutions = () => {
               <div className='year-section'>
                 <div className='year-section-left'>
                   <h3>About Us</h3>
-                  <img src={arrow} alt='Arrow' className='arrow-image' />
+                    <img src={arrow} alt='Arrow' className='arrow-image' />
                   <div className='hover-container'>
                     <div className='hover-icon'>
                     </div>
@@ -126,7 +51,7 @@ const Solutions = () => {
                 <div className='year-section-right'>
                   <h3>About Us</h3>
 
-                  <div className='hover-container'>
+                <div className='hover-container'>
                     <img src={thumb} alt='thumb' className='arrow-image' />
                     <div className='hover-icon'>
                     </div>
@@ -168,129 +93,47 @@ const Solutions = () => {
           </div>
         </div>
         <div className='scroll-wrapper' >
-          <Slider {...settings}>
-            <div>
-              <div className='container ' >
-                <div className='row mt-5'>
-                  <div className='col-md-12'>
+       
+        <div className='container ' >
+          <div className='row mt-5'>
+            <div className='col-md-12'>
+            
+              <div className='solution-text-wrapper mt-5'>
+                <div className='solution-text-wrapper-bg'></div>
+                <div className='solution-text-heading'>
+                  <div>
+                    <h1 className='text-white'>Solutions
+                      To Grow Your
+                      Business</h1>
+                  </div>
+                  <div className='media-query-flex'>
+                    <div className='media-query-circle'>
+                      <img src={round} className='' />
+                    </div>
+                    <div className='circle-arrow'>
+                      <img src={arrow1} className='img-fluid' alt='/' />
+                      <p>Scroll</p>
+                      <br />
 
-                    <div className='solution-text-wrapper mt-5'>
-                      <div className='solution-text-wrapper-bg'></div>
-                      <div className='solution-text-heading'>
-                        <div>
-                          <h1 className='text-white'>Solutions
-                            To Grow Your
-                            Business</h1>
-                        </div>
-                        <div className='media-query-flex'>
-                          <div className='media-query-circle'>
-                            <img src={round} className='' />
-                          </div>
-                          <div className='circle-arrow'>
-                            <img src={arrow1} className='img-fluid' alt='/' />
-                            <p>Scroll</p>
-                            <br />
-
-                            <div className='overflow-blur'>
+                      <div className='overflow-blur'>
 
 
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
-
-
-
                   </div>
                 </div>
               </div>
 
+              
+             
             </div>
-
-            <div>
-              <div className='container ' >
-                <div className='row mt-5'>
-                  <div className='col-md-12'>
-
-                    <div className='solution-text-wrapper mt-5'>
-                      <div className='solution-text-wrapper-bg'></div>
-                      <div className='solution-text-heading'>
-                        <div>
-                          <h1 className='text-white'>Solutions
-                            To Grow Your
-                            Business</h1>
-                        </div>
-                        <div className='media-query-flex'>
-                          <div className='media-query-circle'>
-                            <img src={round} className='' />
-                          </div>
-                          <div className='circle-arrow'>
-                            <img src={arrow1} className='img-fluid' alt='/' />
-                            <p>Scroll</p>
-                            <br />
-
-                            <div className='overflow-blur'>
-
-
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-
-
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div>
-                  <div className='container ' >
-                <div className='row mt-5'>
-                  <div className='col-md-12'>
-
-                    <div className='solution-text-wrapper mt-5'>
-                      <div className='solution-text-wrapper-bg'></div>
-                      <div className='solution-text-heading'>
-                        <div>
-                          <h1 className='text-white'>Solutions
-                            To Grow Your
-                            Business</h1>
-                        </div>
-                        <div className='media-query-flex'>
-                          <div className='media-query-circle'>
-                            <img src={round} className='' />
-                          </div>
-                          <div className='circle-arrow'>
-                            <img src={arrow1} className='img-fluid' alt='/' />
-                            <p>Scroll</p>
-                            <br />
-
-                            <div className='overflow-blur'>
-
-
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-
-
-                  </div>
-                </div>
-                  </div>
-            </div>
-
-
-          </Slider>
-
-
+          </div>
+        </div>
+             
+       
         </div>
 
-
+      
       </div>
     </>
   )
