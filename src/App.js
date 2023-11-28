42438973a199083ab9de2a46840603aa8577d9c4
Ravinder-khera <ravinder.decrypt@gmail.com>
2023-11-28 14:21:49 +0530

    mobile slider added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@
 import './App.css';
 import Footer from './Footer/Footer';
 import Craousel from './component/Craousel/Craousel';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Solution_respo from './component/Sulution_respo/Solution_respo';
 
