101495f17fc5f9a4ca315d240aa7f2b46f7ce5c2
rahul <raahul.decrypt@gmail.com>
2023-12-18 20:21:44 +0530

    18th dec

--- src/pages/Development/Development.jsx
+++ src/pages/Development/Development.jsx
@@ -3,6 +3,10 @@ import './development.scss';
 import CenterMode from '../../component/MySwiper/Swiper'
 import { CheckIcon } from '../../assets/svgIcons';
 const Development = () => {
+    var he = 100;
+    const test = {
+        background:`linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(254,254,254,1) ${he}%, rgba(0,0,0,1) ${he}%, rgba(3,3,3,1) 100%)`,
+    }
     return (
         <>
             <div className='development-banner-section'>
@@ -26,58 +30,167 @@ const Development = () => {
                     sit amet molestie nibh hendrerit eu. Nulla maximus ex non eleifend porttitor.</p>
             </div>
 
-                
-                
+
+            <div className='choose_text'>
+                <h1>Why Choose Us?</h1>
+            </div>
             <div className='chooseUs_section'>
-                
+
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>Transparency</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
 
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>No Themes</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
 
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>Custom design</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
 
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>responsive design</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
 
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>SEO Friendly</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
 
                 <div className='choose_box'>
                     <CheckIcon />
                     <h1>All-in-one</h1>
                     <p>We offer full transparency with our
-                    website design and development processes,
-                    so you know exactly what you are getting.</p>
+                        website design and development processes,
+                        so you know exactly what you are getting.</p>
                 </div>
-               
+
+            </div>
+
+            {/* <div className='process_timeline-bg'>
+                <div className='timeline'>
+                    <ul>
+                        <li>
+                            <div class="timeline-content">
+                                <h1>Consultation & Initial Scope</h1>
+                                <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="timeline-content">
+                                <h1>Heading 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="timeline-content">
+                                <h1>Heading 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="timeline-content">
+                                <h1>Heading 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="timeline-content">
+                                <h1>Heading 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div> */}
+            <div className='process_timeline-bg'>
+
+                <div className="container container1">
+                    <div className="row">
+                        <div className="v-timeline">
+
+                            <div className="v-container rights">
+                                <div className="v-content " >
+                                    <h1>Consultation & Initial Scope</h1>
+                                    <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+                                </div>
+                            </div>
+
+                            <div className="v-container lefts">
+                                <div className="v-content">
+                                    <h1>Website Design Phase </h1>
+                                    <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+                                </div>
+                            </div>
+
+                            <div className="v-container rights">
+                                <div className="v-content">
+                                    <h1>Website Development Phase </h1>
+                                    <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+                                </div>
+                            </div>
+
+                            <div className="v-container lefts">
+                                <div className="v-content">
+                                    <h1>Testing </h1>
+                                    <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+                                </div>
+                            </div>
+
+                            <div className="v-container lefts">
+                                {/* <div className="v-content">
+    <h1>Testing </h1>
+      <p>Our initial stages begin with a consultation with stakeholders and staff to allow for a complete brief of what you after so we can ensure we are on the same page with delivery and expectations.</p>
+    </div> */}
+                            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='bottom_overlay'></div>
+            <div className='process_timeline-bg1'>
+                <div className='blur_text'>
+                    <h1>Website Launch</h1>
+                    <p>Your website is ready, and you new website goes live!</p>
+                </div>
             </div>
         </>
     )
